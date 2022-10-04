500e4888a49d253c25f32b8bb3e3e1fbccb4e9a9
KESHAVBERG <kingkeshav7@gmail.com>
2022-10-05 05:22:00 +0530

    added submit btn

--- src/componets/tests/OralCancer.jsx
+++ src/componets/tests/OralCancer.jsx
@@ -1,28 +1,39 @@
-import React,{useState} from 'react'
-import {Stack, Typography, Button} from '@mui/material';
+import React, { useState } from 'react'
+import { Stack, Typography, Button } from '@mui/material';
 
 const OralCancer = () => {
- const [state, setState] = useState({image:" "});
+  const [state, setState] = useState({ image: " " });
 
-function handleImageChange(e) {
+  function handleImageChange(e) {
     setState({
       image: e.target.files[0]
     })
 
     console.log(state)
   };
+
+  function handelSubmit(e) {
+    e.preventDefault();
+    let imgData = new FormData();
+
+  }
   return (
-    <Stack spacing={2} sx={{pl:"20px"}}>
-       <Typography variant='h4' fontWeight="bold" sx={{}}>
-            Oral Cancer
-        </Typography> 
-        <Typography variant='p' sx={{}}>
-            Upload you image of Cancer in a .jpg format
-        </Typography> 
-        <input type="file" className="input-file" name="imgUpload" accept='.jpg' onChange={handleImageChange}/>
+    <Stack spacing={2} sx={{ pl: "20px" }}>
+      <Typography variant='h4' fontWeight="bold" sx={{}}>
+        Oral Cancer
+      </Typography>
+      <Typography variant='p' sx={{}}>
+        Upload you image of Cancer in a .jpg format
+      </Typography>
+      <form onSubmit="">
+        <input type="file" className="input-file" name="imgUpload" accept='.jpg' onChange={handleImageChange} />
+        <br />
+        <br />
+
+        <input type="submit" style={{ color: "white", backgroundColor: "#157DEC", width: "150px", height: "30px", border: "none" }} />
+      </form>
 
 
-<Button>Sumbit</Button>
     </Stack>
   )
 }
