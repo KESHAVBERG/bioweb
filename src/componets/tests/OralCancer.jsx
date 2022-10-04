e77334851ef2507dd94b5e9075cbaee5e339c89b
KESHAVBERG <kingkeshav7@gmail.com>
2022-10-04 21:01:29 +0530

    got image path

--- src/componets/tests/OralCancer.jsx
+++ src/componets/tests/OralCancer.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Stack, Typography, Button} from '@mui/material';
 
 const OralCancer = () => {
+ const [state, setState] = useState({image:" "});
+
+function handleImageChange(e) {
+    setState({
+      image: e.target.files[0]
+    })
+
+    console.log(state)
+  };
   return (
     <Stack spacing={2} sx={{pl:"20px"}}>
        <Typography variant='h4' fontWeight="bold" sx={{}}>
             Oral Cancer
         </Typography> 
-        <Button variant="contained" style={{width:"200px"}}  sx={{ml:"20px"}}>upload your image</Button>
+        <Typography variant='p' sx={{}}>
+            Upload you image of Cancer in a .jpg format
+        </Typography> 
+        <input type="file" className="input-file" name="imgUpload" accept='.jpg' onChange={handleImageChange}/>
+
+
+<Button>Sumbit</Button>
     </Stack>
   )
 }
