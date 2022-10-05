83068020418c5f93e50a8880653b6f9703d1b2c0
KESHAVBERG <kingkeshav7@gmail.com>
2022-10-05 14:50:40 +0530

    done with oral cancer

--- src/componets/tests/OralCancer.jsx
+++ src/componets/tests/OralCancer.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import { Stack, Typography, Button } from '@mui/material';
 
 const OralCancer = () => {
-  const [state, setState] = useState({image:""});
+  const [state, setState] = useState({ image: "" });
 
+  const [result, setResult] = useState([]);
   function handleImageChange(e) {
     setState({
       image: e.target.files[0]
@@ -13,23 +14,33 @@ const OralCancer = () => {
   function handelSubmit(e) {
     e.preventDefault();
     let form_data = new FormData();
-    console.log(state);
-    form_data.append('image',state);
+    var imagedata = document.querySelector('input[type="file"]').files[0];
+
+    console.log("submit clicked");
+    console.log(imagedata);
+
+    form_data.append('image', imagedata);
+    console.log(form_data.get('image'))
     const options = {
-      mode: "no-cors",
-      method:'POST',
+      // mode: "no-cors",
+      method: 'POST',
       //   headers: {
       //   'Content-Type': 'multipart/form-data',
       // },
       body: form_data
     }
 
-    fetch("http://127.0.0.1:5000/predict/oral_cancer",options).then(res => {
-      console.log(res.data);
-    }).catch(e => console.log(e))
+    fetch("http://127.0.0.1:5000/predict/oral_cancer", options).then((res)=>res.json()).then((data)=>{
+      setResult(data)
+      console.log(result.result)
+    }
+    )
 
 
   }
+
+
+
   return (
     <Stack spacing={2} sx={{ pl: "20px" }}>
       <Typography variant='h4' fontWeight="bold" sx={{}}>
@@ -38,7 +49,7 @@ const OralCancer = () => {
       <Typography variant='p' sx={{}}>
         Upload you image of Cancer in a .jpg format
       </Typography>
-      <form onSubmit={handelSubmit}>
+      <form onSubmit={handelSubmit} method="post">
         <input type="file" className="input-file" name="imgUpload" accept='.jpg' onChange={handleImageChange} />
         <br />
         <br />
@@ -46,6 +57,7 @@ const OralCancer = () => {
         <input type="submit" style={{ color: "white", backgroundColor: "#157DEC", width: "150px", height: "30px", border: "none" }} />
       </form>
 
+  <p>{result.result*100}% change of a cancer</p>
 
     </Stack>
   )
